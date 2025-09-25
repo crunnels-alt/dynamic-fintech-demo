d88be4f6e3ff616710de756081ec8e99d859e39d
crunnels <connorrunnels@gmail.com>
2025-09-24 20:34:28 -0700

    trigger rebuild: update server comment to force Railway redeploy with new env vars

--- server.js
+++ server.js
@@ -2,5 +2,6 @@
 
 // Main server entry point for Railway deployment
 // This file starts the Express application
+// Updated: 2025-01-25 - Trigger rebuild with new Infobip credentials
 
 require('./src/app.js');
